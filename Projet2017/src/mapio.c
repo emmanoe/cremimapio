b5f65f2151cb1e5e887acd55d33fc3c734cb7e87
Sullyvan Tremor <stremor@emi.u-bordeaux1.fr>
2017-11-10 18:05:26 +0100

    save fonctionnel

--- Projet2017/src/mapio.c
+++ Projet2017/src/mapio.c
@@ -9,15 +9,6 @@
 
 #ifdef PADAWAN
 
-
-static void verifier(int cond, char *msg){
-   if(!cond){
-      perror(msg);
-      exit(1);
-   }
-}
-
-
 void map_new (unsigned width, unsigned height)
 {
   map_allocate (width, height);
@@ -44,110 +35,134 @@ void map_new (unsigned width, unsigned height)
   map_object_add ("images/herb.png", 1, MAP_OBJECT_AIR);
   // Petit plancher flottant
   map_object_add ("images/floor.png", 1, MAP_OBJECT_SEMI_SOLID);
-
-  //Les fleurs
+  // Fleur paysage
   map_object_add ("images/flower.png", 1, MAP_OBJECT_AIR);
-  //Les pieces
+  // Pieces à collecter
   map_object_add ("images/coin.png", 20, MAP_OBJECT_AIR | MAP_OBJECT_COLLECTIBLE);
-
+  
 
   map_object_end ();
 }
 
+int len_INT(int i){
+
+  int cpt = 0;
+  ( i < 0 ) ? ( i*(-1) & cpt++) : i;//WARNING
+  do{
+    cpt++;
+  }while( (i/=10) > 0);
+
+  return cpt;
+
+}
+
+void verification(int condition,char* error){
+  
+  if(condition){
+    perror(error);
+    exit(EXIT_FAILURE);
+  }
+  
+}
+
+void add_list(char** list_objts,int current_size,char* obj_name){
+
+  char* error = "add_list";
+  
+  list_objts[current_size] = (char*)malloc(sizeof(char)*strlen(obj_name));
+  verification(list_objts[current_size]==NULL,error);
+  strcpy(list_objts[current_size],obj_name);
+  
+}
+
+void add_tab(char** tab_objts,int current_size,int x,int y,int obj){
+
+  char* error = "add_tab";
+  char obj_name[len_INT(x)+len_INT(y)+len_INT(obj)+4];
+  
+  sprintf(obj_name,"%d %d %d\n",x,y,obj);
+  tab_objts[current_size] = (char*)malloc(sizeof(char)*(len_INT(x)+len_INT(y)+len_INT(obj)+2));
+  verification(tab_objts[current_size]==NULL,error);
+  strcpy(tab_objts[current_size],obj_name);
+  
+}
+
+int isInList(char** list_objts,char* obj_name){
+
+  int i = 0;
+  while(list_objts[i] != NULL){
+    if(!strcmp(list_objts[i],obj_name) ){
+      return 1;
+    }
+    i++;
+  }
+  return 0;
+}
+
 void map_save (char *filename)
 {
-  int out = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0640);
-  if(out !=-1){
-    /* On releves les propriétés qui sont actuellment sur la carte */
-    /*
-      unsigned w = map_width();
-      unsigned h = map_height();
-      int nb_obj = map_objects();
-      int test=0;
-      int get_obj[(map_width()*map_height())];
-      int i=0;
-      char *name=NULL;
-      int nb_sprit=0;
-      int map_obj=0;
-    */
-    
-    
-    
-    /* On ecrit sur le fichier les éléments relevés. */
-      /*
-      test = write(out, &w ,sizeof(w));
-      verifier(test != -1, "Echec width");
-      test = write(out, &h ,sizeof(h));
-      verifier(test != -1, "Echec height");
-      test = write(out, &nb_obj ,sizeof(h));
-      verifier(test != -1, "Echec nbr_object");
-      //Matrice de la map
-      for(int x=0; x<w; x++){
-         for(int y=0; y<h; y++){
-            get_obj[i] = map_get(x,y);
-            test = write(out, &get_obj[i], sizeof(get_obj[i]));
-            verifier(test != -1, "Echec contains");
-            i++;
-         }
+  char* error = "map_save";
+  
+  FILE* fd = fopen(filename,"w");
+  verification(fd==NULL,error);
+  
+  unsigned h = map_height();
+  unsigned w = map_width();
+  unsigned nb_objects = map_objects();
+  fprintf(fd,"%d %d %d\n",h,w,nb_objects);
+
+  int list_len = 0;
+  int tab_len = 0;
+  
+  char** list_objects = (char**) malloc(nb_objects*sizeof(char*));
+  verification(list_objects ==NULL,error);
+  
+  char** tab_objects = (char**) malloc((h*w)*sizeof(char*));
+  verification(tab_objects ==NULL,error);
+  
+  for(int i = 0; i < nb_objects; i++){
+    list_objects[i]=NULL;
+  }
+
+  for(int i = 0; i < h*w; i++){
+    tab_objects[i]=NULL;
+  }
+  
+
+  for(int x = 0; x < w; x++){
+    for(int y = 0; y < h ; y++,tab_len++){
+      int obj = map_get(x,y);
+      add_tab(tab_objects,tab_len,x,y,obj);
+      if( list_len < nb_objects && obj != MAP_OBJECT_NONE && !isInList(list_objects,map_get_name(obj))){
+	add_list(list_objects,list_len,map_get_name(obj));
+	fprintf(fd,"%zu %s %d %d ",strlen(list_objects[list_len]),list_objects[list_len],map_get_frames(obj),map_get_solidity(obj));
+	fprintf(fd,"%d %d %d\n",map_is_destructible(obj),map_is_collectible(obj),map_is_generator(obj));
+	list_len++;
       }
-      //Caracteristiques de chaque type d'objet
-      for(int x=0; x<w; x++){
-         for(int y=0; y<h; y++){
-            name = map_get_name(map_get(x,y));
-            nb_sprit = map_get_frames(map_get(x,y));
-            switch((map_get(x,y)){
-                  case :
-                     break;
-                     default:
-                        break;
+    }
+  }
 
+  for(int i = 0; i < tab_len; i++)//OPTIMISATION : tab write before list; load => lseek jump h*w; ?
+    fprintf(fd,"%s", tab_objects[i]);
 
-               }
-         }
-      }
-      close(out);
-      */
-   }
-   else{
-      perror("Echec open()-> map_save");
-      exit(1);
-   }
+  for(int i=0; i < h*w; i++){
+    if(i < nb_objects){
+      free(list_objects[i]);
+    }
+    free(tab_objects[i]);
+  }
 
-   //fprintf (stderr, "Sorry: Map save is not yet implemented\n");
+  free(list_objects);
+  free(tab_objects);
 
+  printf("Saved in %s\n",filename);
+  
+  fclose(fd);
 }
 
 void map_load (char *filename)
 {
-  int in = open(filename, O_RDONLY);
-  if(in !=-1){
-    /* On releves les propriétés de la map */
-    /*
-      unsigned w, h;
-      read(in, &w ,sizeof(w));
-      read(in, &h ,sizeof(h));
-    */
-    
-    /* On ecrit les éléments relevé sur la maps. */
-    /*
-      map_allocate (w, h);
-      for(int x=0; x<w; x++){
-      for(int y=0; y<h; y++){
-      char c;
-      test = read(in, &c, sizeof(c));
-      verifier(test != -1, "");
-      map_set(x,y, atoi(c));
-      }
-      }
-      
-      close(in);
-    */
-  }
-  else{
-    perror("Echec open()-> map_load");
-    exit(1);
-  }
-  //exit_with_error ("Map load is not yet implemented\n");
+  printf("Loaded since %s\n",filename);
 }
 
 #endif
