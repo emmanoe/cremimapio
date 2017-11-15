0ddb978925416be78c6b7ed3110aac40b3ea74c5
stremor <sullyvan.tremor@etu.u-bordeaux.fr>
2017-11-15 11:55:37 +0100

    Suite de l'implementation des options de maputil

--- Projet2017/util/src/maputil.c
+++ Projet2017/util/src/maputil.c
@@ -6,55 +6,86 @@
 
 #define NB_OPTIONS 8
 
-#define opt1 "--getinfo"
-#define opt2 "--getheight"
-#define opt3 "--getwidth"
-#define opt4 "--getobjects"
+#define POS_height 0
+#define POS_width 1
+#define POS_objects 2
+
+#define opt0 "--getinfo"
+#define opt1 "--getheight"
+#define opt2 "--getwidth"
+#define opt3 "--getobjects"
+#define opt4 "--setheight"
 #define opt5 "--setwidth"
-#define opt6 "--setheight"
-#define opt7 "--setobjects"
-#define opt8 "--pruneobjects"
+#define opt6 "--setobjects"
+#define opt7 "--pruneobjects"
 
 int main(int argc, char* argv[])
 {
     
     char* error = "maputil";
     
-    if(argc == 3){
+    if(argc > 2){
         
         int fd = open(argv[1],O_RDWR);
         //verification( fd == -1,error);
         
-        char* valid_options[NB_OPTIONS]={opt1,opt2,opt3,opt4,opt5,opt6,opt7,opt8};
+        char* valid_options[NB_OPTIONS]={opt0,opt1,opt2,opt3,opt4,opt5,opt6,opt7};
         int val;
-        
         int all_opt = 0;
-        if(!strcmp(argv[2],valid_options[0]))
-            all_opt = 1;
+        int valid = lseek(fd,0,SEEK_CUR);
         
-        if(!strcmp(argv[2],valid_options[1]) || all_opt ){
-            lseek(fd,0*sizeof(unsigned),SEEK_SET);
-            read(fd,&val,sizeof(unsigned));
-            printf("height : %d\n",val);
+        if(argc == 3){
+            
+            if(!strcmp(argv[2],valid_options[0]))
+                all_opt = 1;
+            
+            if(!strcmp(argv[2],valid_options[1]) || all_opt ){
+                lseek(fd,POS_height*sizeof(unsigned),SEEK_SET);
+                read(fd,&val,sizeof(unsigned));
+                printf("height : %d\n",val);
+            }
+            
+            if(!strcmp(argv[2],valid_options[2]) || all_opt ){
+                lseek(fd,POS_width*sizeof(unsigned),SEEK_SET);
+                read(fd,&val,sizeof(unsigned));
+                printf("width : %d\n",val);
+            }
+            
+            if(!strcmp(argv[2],valid_options[3]) || all_opt){
+                lseek(fd,POS_objects*sizeof(unsigned),SEEK_SET);
+                read(fd,&val,sizeof(unsigned));
+                printf("objects : %d\n",val);
+            }
+            
+            if(valid == lseek(fd,0,SEEK_CUR)){
+                fprintf(stderr,"invalid option : %s\n",argv[2]);
+                return EXIT_FAILURE;
+            }
+                
+        }else if(argc==4){
+            
+            val = atoi(argv[3]);
+            
+            if(!strcmp(argv[2],valid_options[4]))
+                lseek(fd,POS_height*sizeof(unsigned),SEEK_SET);
+            
+            if(!strcmp(argv[2],valid_options[5]))
+                lseek(fd,POS_width*sizeof(unsigned),SEEK_SET);
+            
+            if(valid != lseek(fd,0,SEEK_CUR) || (!strcmp(argv[2],valid_options[4])))
+                write(fd,&val,sizeof(unsigned));
+            else{
+                fprintf(stderr,"invalid option : %s\n",argv[2]);
+                return EXIT_FAILURE;
+            }
+            
         }
-        
-        if(!strcmp(argv[2],valid_options[2]) || all_opt ){
-            lseek(fd,1*sizeof(unsigned),SEEK_SET);
-            read(fd,&val,sizeof(unsigned));
-            printf("width : %d\n",val);
-        }
-        
-        if(!strcmp(argv[2],valid_options[3]) || all_opt){
-            lseek(fd,2*sizeof(unsigned),SEEK_SET);
-            read(fd,&val,sizeof(unsigned));
-            printf("objects : %d\n",val);
+        else{
+            fprintf(stderr,"error arguments\n");
+            return EXIT_FAILURE;
         }
         
         close(fd);
-        
-    }else{
-        fprintf(stderr,"error arguments\n");
-        return EXIT_FAILURE;
     }
     
     return 0;
