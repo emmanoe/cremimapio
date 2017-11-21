ee2073e710049c19f8fb956a334fad1817c9d110
Sullyvan Tremor <stremor@emi.u-bordeaux1.fr>
2017-11-21 14:23:46 +0100

    Elargissement de la maps

--- Projet2017/util/src/maputil.c
+++ Projet2017/util/src/maputil.c
@@ -8,7 +8,7 @@
 #include "../../include/map.h"
 #include "../../include/error.h"
 
-#define NB_OPTIONS 8
+#define NB_OPTIONS 9
 
 #define POS_height 0
 #define POS_width 1
@@ -21,8 +21,9 @@
 #define opt3 "--getobjects" //ok
 #define opt4 "--setheight" //ok
 #define opt5 "--setwidth" //OK
-#define opt6 "--setobjects"
+#define opt6 "--setobjects" //ok
 #define opt7 "--pruneobjects" //ok
+#define opt8 "--getpresence" //ok
 
 off_t get(int fd,int* val,char* s,int depl,int act){
     lseek(fd,depl*sizeof(unsigned),SEEK_SET);
@@ -110,7 +111,7 @@ int main(int argc, char* argv[]){
     
     if(argc > 2){
         
-        char* valid_options[NB_OPTIONS]={opt0,opt1,opt2,opt3,opt4,opt5,opt6,opt7};
+      char* valid_options[NB_OPTIONS]={opt0,opt1,opt2,opt3,opt4,opt5,opt6,opt7,opt8};
         
         int verif = 0;
         for(int i = 0; i < NB_OPTIONS; i++){
@@ -130,10 +131,14 @@ int main(int argc, char* argv[]){
         int all_opt = 0;
         int valid = lseek(fd,0,SEEK_CUR);
         int x,y,obj;
-        
+
+	int current_height;
+        int current_width;
         int current_objects;
         int current_presence;
-        
+
+	get(fd,&current_height,"",POS_height ,0);
+        get(fd,&current_width,"",POS_width,0);
         get(fd,&current_objects,"",POS_objects,0);
         get(fd,&current_presence,"",POS_presence,0);
         
@@ -152,6 +157,10 @@ int main(int argc, char* argv[]){
             
             if(!strcmp(argv[2],valid_options[3]) || all_opt){
                 get(fd,&val,"objects",POS_objects,1);
+            }
+
+	    if(!strcmp(argv[2],valid_options[8]) || all_opt){
+                get(fd,&val,"presence",POS_presence,1);
             }
             
             if((!strcmp(argv[2],valid_options[7])) ){
@@ -262,14 +271,25 @@ int main(int argc, char* argv[]){
                 int fd2 = open(argv[1],O_RDONLY);
                 getCoords(fd,current_objects);
                 getCoords(fd2,current_objects);
+
+		int act = 1;
+		int save_obj;
                 
                 for(int i = 0; i < current_presence; i++){
                     
                     int cmp;
+		    
                     
-                    read(fd,&x,sizeof(int));
-                    read(fd,&y,sizeof(int));
-                    read(fd,&obj,sizeof(int));
+                    read(fd2,&x,sizeof(int));
+                    read(fd2,&y,sizeof(int));
+                    read(fd2,&obj,sizeof(int));
+
+		    if(act){
+		      if(( depl == POS_height && y == 0 && x == 0 ) || ( depl == POS_width && y == old_value-1 && x == 0 )){
+                        save_obj = obj;
+			act=0;
+		      }
+		    }
                     
                     if( depl == POS_height ){
                         cmp = y;
@@ -277,13 +297,9 @@ int main(int argc, char* argv[]){
                         cmp = x;
                     }
                     
-                    //lseek(fd,-3*sizeof(int),SEEK_CUR);
-                    
                     int diff = (old_value-val);
                     if( old_value > val){//Retrecisseent
                         if( (cmp-diff < 0 && depl == POS_height) || ( cmp > val && depl == POS_width) ){
-			  //x = -1;
-			  //y =-1;
                             cpt++;
                             continue;
                             
@@ -307,6 +323,41 @@ int main(int argc, char* argv[]){
                 }
                 close(fd2);
                 ftruncate(fd,lseek(fd,0,SEEK_CUR));
+
+		if(val-old_value > 0){
+		  obj = save_obj;
+
+		  if(depl == POS_height){
+		    for(int y = 0; y < val-old_value; y++){
+
+		      x = 0;
+		      write(fd,&x,sizeof(int));
+		      write(fd,&y,sizeof(int));
+		      write(fd,&obj,sizeof(int));
+
+		      x = current_width-1;
+		      write(fd,&x,sizeof(int));
+		      write(fd,&y,sizeof(int));
+		      write(fd,&obj,sizeof(int));
+		      
+		      current_presence+=2;
+		    }
+		  }else{
+
+		    for(int x = old_value; x < val; x++){
+
+		      y = current_height-1 ;
+		      write(fd,&x,sizeof(int));
+		      write(fd,&y,sizeof(int));
+		      write(fd,&obj,sizeof(int));
+
+		      current_presence++;
+		    }
+		    
+		  }
+		  
+		  
+		}
                 
                 current_presence-= cpt;
                 set(fd,&current_presence,POS_presence);
