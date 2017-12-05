ef2a8cab95db23c4ed5e5a9979b663c77a5a0fb0
Sullyvan Tremor <stremor@emi.u-bordeaux1.fr>
2017-12-05 18:18:42 +0100

    Correction du commit prec

--- Projet2017/src/tempo.c
+++ Projet2017/src/tempo.c
@@ -9,14 +9,28 @@
 
 #include "timer.h"
 
+// Return number of elapsed µsec since... a long time ago
+static unsigned long get_time (void)
+{
+  struct timeval tv;
+
+  gettimeofday (&tv ,NULL);
+
+  // Only count seconds since beginning of 2016 (not jan 1st, 1970)
+  tv.tv_sec -= 3600UL * 24 * 365 * 46;
+  
+  return tv.tv_sec * 1000000UL + tv.tv_usec;
+}
+
+#ifdef PADAWAN
+
+
 void handler(int sig){
-  printf("reçu depuis dans %ld\n",(long)pthread_self());
+  printf ("sdl_push_event(%p) appelée au temps %ld\n",NULL, get_time ());//A CHANGER
 }
 
 void* run_th1(void* theSignal)
-{
-
-  printf("thread %ld : je tourne\n",(long)pthread_self());  
+{ 
 
   struct sigaction gestionSignal;
   
@@ -33,27 +47,11 @@ void* run_th1(void* theSignal)
  
 }
 
-// Return number of elapsed µsec since... a long time ago
-static unsigned long get_time (void)
-{
-  struct timeval tv;
-
-  gettimeofday (&tv ,NULL);
-
-  // Only count seconds since beginning of 2016 (not jan 1st, 1970)
-  tv.tv_sec -= 3600UL * 24 * 365 * 46;
-  
-  return tv.tv_sec * 1000000UL + tv.tv_usec;
-}
-
-#ifdef PADAWAN
-
 // timer_init returns 1 if timers are fully implemented, 0 otherwise
 int timer_init (void)
 {
   pthread_t th1;
   printf(" PID : %d\n",getpid());
-  printf("thread %ld\n",(long)pthread_self());
 
   sigset_t mask;
      sigemptyset(&mask);
@@ -70,6 +68,7 @@ int timer_init (void)
 
 timer_id_t timer_set (Uint32 delay, void *param)
 {
+  
   struct itimerval it_val;
 
   it_val.it_value.tv_sec =     delay/1000;
