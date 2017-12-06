d64aa62ea966ed37676bea7395b965ad4088118a
stremor <sullyvan.tremor@etu.u-bordeaux.fr>
2017-12-06 03:59:02 +0100

    Implementation des listes

--- Projet2017/src/liste.c
+++ Projet2017/src/liste.c
@@ -1,101 +1,159 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "liste.h"
 
-struct List_ctrl{
-	liste_t debut;
-	liste_t fin;
-	int nb;
+struct eventList{
+    int delay;
+    void* param;
+    struct eventList* next;
+    struct eventList* prev;
+};
+typedef struct eventList* list;
+
+
+struct ctrlList{
+    list debut;
+    list fin;
+    int size;
+};
+typedef struct ctrlList* ctrl;
+
+void initCtrl(ctrl c){
+    c->debut=NULL;
+    c->fin=NULL;
+    c->size=0;
 }
 
-struct Liste_t{
-	int delay;
-	void* param;
-	struct Liste_t* next;
+list alloc(int delay, void* param){
+    
+    list new = (list)malloc(sizeof(struct eventList));
+    new->delay=delay;
+    new->param=param;
+    
+    return new;
 }
 
-List_ctrl
-liste_ctrl_alloc(void){
-	List_ctrl l;
-	assert(l = malloc(sizeof(struct List_ctrl)));
-	l->debut = NULL;
-	l->fin = NULL;
-	return l;
+void addTop(ctrl c,int delay, void* param){
+    list new = alloc(delay,param);
+    
+    if(c->size == 0){
+        c->fin = new;
+        new->next = NULL;
+        
+    }else{
+        new->next = c->debut;
+        c->debut->prev=new;
+    }
+    c->debut = new;
+    new->prev = NULL;
+    
+    c->size++;
 }
 
-Liste_t
-liste_alloc(void){
-	Liste_t l;
-	assert(l = malloc(sizeof(struct Liste_t)));
-	l->next = NULL;
-	return l;
+void addAfter(ctrl c,int delay, void* param,list after){
+    
+    list new = alloc(delay,param);
+    if(c->size == 0){
+        c->fin = new;
+        c->debut = new;
+        new->prev = NULL;
+        new->next = NULL;
+    }else{
+        new-> next = after->next;
+        new->prev = after;
+        if(after->next != NULL){
+            after->next->prev = new;
+        }else{
+            c->fin = new;
+        }
+        after->next = new;
+    }
+    
+    c->size++;
 }
 
-void
-liste_free_elem(Liste_t elem){
-	if(elem != NULL)
-		free(elem);
+void addBefore(ctrl c,int delay, void* param,list before){
+    list new = alloc(delay,param);
+    if(c->size == 0){
+        c->fin = new;
+        c->debut = new;
+        new->prev = NULL;
+        new->next = NULL;
+    }else{
+        
+        new-> next = before;
+        new->prev = before->prev;
+        if(before->prev!=NULL){
+            before->prev->next=new;
+        }else{
+            c->debut = new;
+        }
+        before->prev = new;
+        
+    }
+    
+    c->size++;
 }
 
-Liste_t
-liste_prepend(Liste_t L, int delay, void* param, Liste_t next){
-	List_ctrl new = liste_alloc();
-	assert(new);
-	new->delay = delay;
-	new->param = param;
-	new->next = L;
-	return new;
+void addBottom(ctrl c,int delay, void* param){
+    list new = alloc(delay,param);
+    if(c->size == 0){
+        c->fin = new;
+        c->debut = new;
+        new->prev = NULL;
+    }else{
+        new->prev = c->fin;
+        c->fin->next = new;
+        c->fin=new;
+    }
+    new->next = NULL;
+    c->size++;
 }
 
-Liste_t 
-liste_delete_first(Liste_t liste){
-	if(liste != NULL){
-		Liste_t tmp = liste;
-		liste = liste->next;
-		liste_free_elem(tmp);
-	}
-	return liste;
+void delEmpty(ctrl c){
+    
+    c->debut = NULL;
+    c->fin = NULL;
+    c->size--;
 }
 
-Liste_t 
-liste_insert_after(Liste_t l, Liste_t p, int delay, void* param, Liste_t next){
-	if(p!=NULL){
-		Liste_t new = liste_alloc();
-		new->delay = delay;
-		new->param = param;
-		new->next = p->next;
-		p->next = new;
-	}
-	return l;
+void delTop(ctrl c){
+    list del = c->debut;
+    
+    c->debut = c->debut->next;
+    if(c->debut != NULL){
+        c->debut->prev = NULL;
+        c->size--;
+    }
+    else
+        delEmpty(c);
+    free(del);
 }
 
-Liste_t 
-liste_delete_after(Liste_t l, Liste_t p){
-	if(p!=NULL){
-		if(p->next!=NULL){
-			Liste_t tmp = p->next;
-			p->next=p->next->next;
-			liste_free_elem(tmp);
-		}
-	}
-	return L;
+void globalAdd(ctrl c,int delay, void* param){
+    
+    list tmp = c->debut;
+    
+    while(tmp != NULL && delay > tmp->delay ){
+        tmp = tmp->next;
+    }
+    
+    if(tmp == NULL && c->size == 0){
+        addTop(c,delay,param);
+    }else if(tmp != NULL ){
+        if(delay <= tmp->delay && tmp == c->debut){
+            addTop(c,delay,param);
+        }else{
+            addBefore(c,delay,param,tmp);
+        }
+    }else{
+        addBottom(c,delay,param);
+    }
 }
 
-Liste_t 
-liste_next(Liste_t l){
-	if(l->next != NULL){
-		return l->next;
-	}
-	return NULL;
-}
-
-void * liste_param(List_t l){
-	assert(L);
-	return(l->param);
-}
-
-int liste_delay(List_t l){
-	assert(L);
-	return(l->delay);
+void* headList(ctrl c){
+    
+    void *param = c->debut->param;
+    delTop(c);
+    return param;
 }
