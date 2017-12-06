37638335c574747b84d7dd50bff74bd8889a4c06
emmanoe_delar <emmanoe.delar@u-bordeaux.fr>
2017-12-06 10:42:03 +0100

    adaptation liste.h

--- Projet2017/src/liste.h
+++ Projet2017/src/liste.h
@@ -2,31 +2,29 @@
 
 #ifndef LISTE_H
 #define LISTE_H
-typedef struct Liste_t *Liste_t;
-typedef struct List_ctrl *List_ctrl; 
+typedef struct eventList* list;
+typedef struct ctrlList* ctrl;
 
 /* **************************************************************** */
-extern List_ctrl liste_ctrl_alloc(void);
+extern void initCtrl(ctrl c);
 
-extern Liste_t liste_alloc(void);
+extern list alloc(int delay, void* param);
 
-extern void liste_ctrl_free_elem(List_ctrl elem);
+extern void addTop(ctrl c,int delay, void* param);
 
-extern void liste_free_elem(Liste_t elem);
+extern void addAfter(ctrl c,int delay, void* param,list after);
 
-extern Liste_t liste_prepend(Liste_t L, int delay, void* param);
+extern void addBefore(ctrl c,int delay, void* param,list before);
 
-extern Liste_t liste_delete_first(Liste_t liste);
+extern void addBottom(ctrl c,int delay, void* param);
 
-extern Liste_t liste_insert_after(Liste_t l, Liste_t p, int delay, void* param, Liste_t next);
+extern void delEmpty(ctrl c);
 
-extern Liste_t liste_delete_after(Liste_t l, Liste_t p);
+extern void delTop(ctrl c);
 
-extern Liste_t liste_next(Liste_t l);
+extern void globalAdd(ctrl c,int delay, void* param);
 
-extern void * liste_param(Liste_t l);
-
-extern int liste_delay(Liste_t l);
+extern void headList(ctrl c);
 
 /* **************************************************************** */
 
