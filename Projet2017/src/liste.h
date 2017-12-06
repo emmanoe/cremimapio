c638371c2ec87650a9c6b1ff117c13cc20b79034
emmanoe_delar <emmanoe.delar@u-bordeaux.fr>
2017-12-06 12:21:52 +0100

    void* headList

--- Projet2017/src/liste.h
+++ Projet2017/src/liste.h
@@ -16,15 +16,17 @@ extern void addAfter(ctrl c,int delay, void* param,list after);
 
 extern void addBefore(ctrl c,int delay, void* param,list before);
 
+/*Add a new element at the end of the list and incr c */
 extern void addBottom(ctrl c,int delay, void* param);
 
 extern void delEmpty(ctrl c);
 
 extern void delTop(ctrl c);
 
+/*Add a new element according to his delay*/
 extern void globalAdd(ctrl c,int delay, void* param);
 
-extern void headList(ctrl c);
+extern void* headList(ctrl c);
 
 /* **************************************************************** */
 
